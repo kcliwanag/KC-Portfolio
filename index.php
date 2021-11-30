5c6f1bfff81a7fcf58fc8d841802cf2c23ddf4c2
KC Liwanag <kateclarenceliwanag@gmail.com>
2021-11-30 12:02:02 -0800

    Homepage animation edit

--- index.php
+++ index.php
@@ -2,18 +2,17 @@
 
 <div data-aos="fade-up">
   <section>
-    <div class="home-intro container">
-      <div class="lkj"><h1>Captivating <br> Modern Audiences Through</h1></div> 
-      <div class="poi"> 
+      <div class="home-intro container">
+      <div class="statement"><h1>Crafting empathy-driven solutions as a</h1></div>
+      <div class="animated-word"> 
         <h1>
           <ul class="flip4"> 
-            <li>Visual StoryTelling</li>
-            <li>Cute Corgi</li>  
-            <li>Simple UX</li> 
-            <li>Awesome UI</li>
+            <li>UX/UI designer.</li>
+            <li>visual designer.</li>  
+            <li>visual storyteller.</li> 
           </ul>
         </h1>
-      </div>
+      </div> 
       <a class="ca3-scroll-down-link ca3-scroll-down-arrow" data-ca3_iconfont="ETmodules" data-ca3_icon=""></a>
     </div>
   </section>
