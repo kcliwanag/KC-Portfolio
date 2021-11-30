b4740019d02c17db66911dd9e3530fd4babb823e
KC Liwanag <kateclarenceliwanag@gmail.com>
2021-11-30 11:27:32 -0800

    Animation homepage

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
 <?php require_once("partials/header.php"); ?>
 
 <div data-aos="fade-up">
-<section>
-  <div class="home-intro container">
-      <h1>Captivating <br> Modern Audiences<br>Through <span>Visual Storytelling</span></h1>
+  <section>
+    <div class="home-intro container">
+      <div class="lkj"><h1>Captivating <br> Modern Audiences Through</h1></div> 
+      <div class="poi"> 
+        <h1>
+          <ul class="flip4"> 
+            <li>Visual StoryTelling</li>
+            <li>Cute Corgi</li>  
+            <li>Simple UX</li> 
+            <li>Awesome UI</li>
+          </ul>
+        </h1>
+      </div>
       <a class="ca3-scroll-down-link ca3-scroll-down-arrow" data-ca3_iconfont="ETmodules" data-ca3_icon=""></a>
-  </div>
-
-
-</section>
+    </div>
+  </section>
 </div>
 
 
